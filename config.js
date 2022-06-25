2502aa3c5465d579c0e6f6f3d306a82365248fe5
AinulBotz <107542885+AinulBotz@users.noreply.github.com>
2022-06-25 09:42:23 +0800

    Update config.js

--- config.js
+++ config.js
@@ -2,9 +2,9 @@ let fs = require('fs')
 let chalk = require('chalk')
 
 global.owner = [
-  ['62895330379186'],
-  ['62895330379186'],
-  ['62895330379186', 'Owner', true]
+  ['6285754202785'],
+  ['628971724481'],
+  ['6285754202785', 'Owner', true]
   // [number, dia creator/owner?, dia developer?]
 ] // Put your number here
 global.mods = [] // Want some help?
@@ -56,9 +56,9 @@ fs.watchFile(file_exif, () => {
 })
 
 // Sticker WM
-global.packname = sticker_name
-global.author = sticker_author
-global.wm = '© Aine'
+global.packname = Ainul
+global.author = Ainul
+global.wm = '© Di Buat Oleh AinulBotz'
 
 global.multiplier = 1000 // The higher, The harder levelup
 
